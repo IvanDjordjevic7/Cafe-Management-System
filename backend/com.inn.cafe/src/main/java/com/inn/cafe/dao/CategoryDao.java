c11b3335f16f1a337b9666095f8a8de77c00329b
Ivan Djordjeivc <ivandordevic33@gmail.com>
2024-09-25 18:03:12 +0200

    Add Category class and API endpoints for managing categories
    - Implemented endpoint for adding a new category
    - Added endpoint for retrieving a list of all categories
    - Implemented endpoint for updating an existing category

---
+++ backend/com.inn.cafe/src/main/java/com/inn/cafe/dao/CategoryDao.java
@@ -0,0 +1,13 @@
+package com.inn.cafe.dao;
+
+import com.inn.cafe.POJO.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CategoryDao extends JpaRepository<Category, Integer> {
+
+    List<Category> getAllCategory();
+
+}
